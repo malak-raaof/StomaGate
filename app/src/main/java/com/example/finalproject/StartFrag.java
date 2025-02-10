42423ea538359d2c268c7c9df4e61e86c4e978b3
Luka <M.R.SAAD@student.aast.edu>
2025-02-10 22:20:01 +0200

    finalizing the login page ui i guess....i guess....i guess........

--- app/src/main/java/com/example/finalproject/StartFrag.java
+++ app/src/main/java/com/example/finalproject/StartFrag.java
@@ -3,16 +3,18 @@ package com.example.finalproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class StartFrag extends Fragment {
 
 
-
+    Button btnStart;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,13 @@ public class StartFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        btnStart = view.findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_startFrag_to_loginFrag);
+
+        });
+        return view;
     }
 }
