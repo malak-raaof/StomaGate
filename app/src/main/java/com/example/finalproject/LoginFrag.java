d95bf0a75bf83eb040c61bf917bf18ce01516a7c
Luka <M.R.SAAD@student.aast.edu>
2025-02-11 22:37:06 +0200

    sign up ui....done

--- app/src/main/java/com/example/finalproject/LoginFrag.java
+++ app/src/main/java/com/example/finalproject/LoginFrag.java
@@ -9,6 +9,7 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -19,6 +20,8 @@ public class LoginFrag extends Fragment {
     TextInputEditText txtPassword;
 
     TextInputLayout textField2;
+
+    TextView txtSignupClick;
 
 
     @Override
@@ -34,6 +37,12 @@ public class LoginFrag extends Fragment {
         View view = inflater.inflate(R.layout.fragment_login_me, container, false);
         txtPassword = view.findViewById(R.id.txtPassword);
         textField2 = view.findViewById(R.id.textField2);
+        txtSignupClick = view.findViewById(R.id.txtSignupClick);
+
+        txtSignupClick.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_loginFrag_to_signupFrag);
+
+        });
 
 
         return view;
--- app/src/main/java/com/example/finalproject/SignupFrag.java
+++ app/src/main/java/com/example/finalproject/SignupFrag.java
@@ -3,58 +3,36 @@ package com.example.finalproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignupFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignupFrag() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignupFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignupFrag newInstance(String param1, String param2) {
-        SignupFrag fragment = new SignupFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView txtLoginClick;
+    Button btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        txtLoginClick = view.findViewById(R.id.txtLoginClick);
+
+        txtLoginClick.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_signupFrag_to_loginFrag);
+
+        });
+        return view;
     }
 }
